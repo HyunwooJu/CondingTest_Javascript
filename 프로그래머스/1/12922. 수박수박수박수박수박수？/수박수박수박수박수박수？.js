1341f849a113799ce7de7ca6e26b113984719e09
HyunwooJu <37990421+HyunwooJu@users.noreply.github.com>
2024-05-16 11:09:18 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,11 +1,4 @@
 function solution(n) {
-    var answear = '';
-    for (var i = 0; i < n; i++) {
-        if (i % 2 === 0) {
-            answear += '수';
-        } else {
-            answear += '박';
-        }
-    }
-    return answear;
+    var answer = '';
+    return answer;
 }
